3ce239de439d97d955d1471b4572bbb83f7904fe
Ben Jesaulenko <design@upromotions.com.au>
2022-01-13 14:37:15 +1100

    commented nuxt in gitignore

---
+++ .nuxt/components/index.js
@@ -0,0 +1,41 @@
+export { default as BackToTop } from '../..\\components\\back-to-top.vue'
+export { default as CtaSection } from '../..\\components\\cta-section.vue'
+export { default as FooterSection } from '../..\\components\\footer-section.vue'
+export { default as IndexFeatures } from '../..\\components\\index-features.vue'
+export { default as IndexHero } from '../..\\components\\index-hero.vue'
+export { default as Navbar } from '../..\\components\\navbar.vue'
+export { default as NuxtLogo } from '../..\\components\\NuxtLogo.vue'
+export { default as PageHero } from '../..\\components\\page-hero.vue'
+export { default as ScrollDown } from '../..\\components\\scroll-down.vue'
+export { default as VertScroll } from '../..\\components\\vert-scroll.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
---
+++ .nuxt/components/readme.md
@@ -0,0 +1,18 @@
+# Discovered Components
+
+This is an auto-generated list of components discovered by [nuxt/components](https://github.com/nuxt/components).
+
+You can directly use them in pages and other components without the need to import them.
+
+**Tip:** If a component is conditionally rendered with `v-if` and is big, it is better to use `Lazy` or `lazy-` prefix to lazy load.
+
+- `<BackToTop>` | `<back-to-top>` (components/back-to-top.vue)
+- `<CtaSection>` | `<cta-section>` (components/cta-section.vue)
+- `<FooterSection>` | `<footer-section>` (components/footer-section.vue)
+- `<IndexFeatures>` | `<index-features>` (components/index-features.vue)
+- `<IndexHero>` | `<index-hero>` (components/index-hero.vue)
+- `<Navbar>` | `<navbar>` (components/navbar.vue)
+- `<NuxtLogo>` | `<nuxt-logo>` (components/NuxtLogo.vue)
+- `<PageHero>` | `<page-hero>` (components/page-hero.vue)
+- `<ScrollDown>` | `<scroll-down>` (components/scroll-down.vue)
+- `<VertScroll>` | `<vert-scroll>` (components/vert-scroll.vue)
