a42dce1113f6a0ee7e09ef552139e3caeed763bc
Nick Nedelkovski <98880119+UpSkilloz@users.noreply.github.com>
2022-02-05 12:12:22 +1100

    Update schedule-form.vue

--- components/schedule-form.vue
+++ components/schedule-form.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="scheduleForm">
-    <form action="https://form.taxi/s/dfwkk36o" method="POST">
-    <input type="text" name="Name">
-    <input type="email" name="Email">
-    <textarea name="Message"></textarea>
-  <input type="submit" value="Send">
+    <form action="SEND ADDRESS" id="ft-form" method="POST" accept-charset="UTF-8">
+  <fieldset>
+    <label>
+      Name
+      <input type="text" name="name" required>
+    </label>
+    <label>
+      Email address
+      <input type="email" name="email" required>
+    </label>
+    <label>
+      Your message
+      <textarea rows="3" name="message" required></textarea>
+    </label>
+  </fieldset>
+  <div class="btns">
+    <input type="text" name="_gotcha" value="" style="display:none;">
+    <input type="submit" value="Submit">
+  </div>
 </form>
   </div>
 </template>
